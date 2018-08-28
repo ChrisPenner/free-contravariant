0399ddbb53c55fd50cd09fe2f0b6ae7562468925
Chris Penner <christopher.penner@gmail.com>
2018-08-27 22:54:07 -0600

    Try other Divisible instance

--- src/FreeC.hs
+++ src/FreeC.hs
@@ -27,53 +27,77 @@ instance Functor f => Contravariant (FC f) where
 
 instance Alternative f => Divisible (FC f) where
   divide :: forall a b c. (a -> (b, c)) -> FC f b -> FC f c -> FC f a
-  divide split (FC cvb) (FC cvc) = FC (thingB <$> cvb <|> thingC <$> cvc)
-    where
-      thingB :: (b -> FC f b) -> (a -> FC f a)
-      thingB fb =
-        let toB :: a -> b
-            toB = fst <$> split
-            t :: FC f b -> FC f a
-            t = contramap toB
-         in (\a -> t . fb . toB $ a)
-      thingC :: (c -> FC f c) -> (a -> FC f a)
-      thingC fc =
-        let toC :: a -> c
-            toC = snd <$> split
-            t :: FC f c -> FC f a
-            t = contramap toC
-         in (\a -> t . fc . toC $ a)
+  divide split (FC cvb) (FC cvc) =
+    FC . pure $ \a ->
+      let (b, c) = split a
+          FC bbb = contramap (fst . split) (FC cvb)
+          FC ccc = contramap (snd . split) (FC cvc)
+       in FC (bbb <|> ccc)
+  -- divide split (FC cvb) (FC cvc) = FC ((thingB <$> cvb) <|> (thingC <$> cvc))
+  --   where
+  --     thingB :: (b -> FC f b) -> (a -> FC f a)
+  --     thingB fb =
+  --       let toB :: a -> b
+  --           toB = fst <$> split
+  --           t :: FC f b -> FC f a
+  --           t = contramap toB
+  --        in (\a -> t . fb . toB $ a)
+  --     thingC :: (c -> FC f c) -> (a -> FC f a)
+  --     thingC fc =
+  --       let toC :: a -> c
+  --           toC = snd <$> split
+  --           t :: FC f c -> FC f a
+  --           t = contramap toC
+  --        in (\a -> t . fc . toC $ a)
   conquer :: FC f a
   conquer = FC empty
--- instance Alternative f => Decidable (FC f) where
---   lose :: (a -> Void) -> FC f a
---   lose f = FC (absurd <$> f)
---   choose :: (a -> Either b c) -> FC f b -> FC f c -> FC f a
---   choose choice (FC fb) (FC fc) =
---     FC $ \a ->
---       case choice a of
---         Left b -> flip (choose choice) (FC fc) <$> (fb b)
---         Right c -> choose choice (FC fb) <$> (fc c)
--- step :: a -> FC f a -> f (FC f a)
--- step = flip runFC
--- joinStep :: Monad m => a -> FC m a -> FC m a
--- joinStep a (FC m) =
---   FC $ \a' -> do
---     FC m' <- m a
---     m' a'
--- aStep :: Alternative m => a -> FC m a -> FC m a
--- aStep a (FC m) =
---   FC $ \a' ->
---     let x = m a
---      in x
--- extractStep :: Comonad w => a -> FC w a -> FC w a
--- extractStep a (FC wa) = extract (wa a)
--- failTrue :: FC Maybe Bool
--- failTrue =
---   FC $ \case
---     True -> Nothing
---     False -> Just failTrue
--- hoist :: Functor g => (forall a. f a -> g a) -> FC f a -> FC g a
--- hoist f (FC cfa) = FC $ fmap (hoist f) . f . cfa
--- count :: FC (Compose [] ((,) Int)) ()
--- count = FC $ \() -> Compose [(1, count), (2, count)]
+
+instance Alternative f => Decidable (FC f) where
+  lose :: (a -> Void) -> FC f a
+  lose f = FC (pure (absurd . f))
+  choose :: forall a b c. (a -> Either b c) -> FC f b -> FC f c -> FC f a
+  choose choice (FC fb) (FC fc) = FC $ liftA2 makeAChoice fb fc
+    where
+      makeAChoice :: (b -> FC f b) -> (c -> FC f c) -> (a -> FC f a)
+      makeAChoice fb' fc' a =
+        case choice a of
+          Left b -> choose choice (fb' b) (FC fc)
+          Right c -> choose choice (FC fb) (fc' c)
+
+step :: (Functor f) => a -> FC f a -> f (FC f a)
+step a (FC fa) = fmap ($ a) fa
+
+joinStep :: Monad m => a -> FC m a -> FC m a
+joinStep a (FC m) =
+  FC $ do
+    fa <- m
+    runFC $ fa a
+
+extractStep :: Comonad w => a -> FC w a -> FC w a
+extractStep a (FC wa) = extract wa a
+
+failTrue :: FC Maybe Bool
+failTrue =
+  FC $
+  Just $ \case
+    True -> FC Nothing
+    False -> failTrue
+
+hoist :: Functor f => (forall a. f a -> g a) -> FC f a -> FC g a
+hoist f (FC cfa) = FC $ f (fmap (hoist f) <$> cfa)
+
+failNegative :: FC (Either String) Int
+failNegative = contramap (< 0) $ hoist (toEither "negative") failTrue
+
+toEither :: String -> Maybe a -> Either String a
+toEither e Nothing = Left e
+toEither _ (Just r) = Right r
+
+failEven :: FC (Either String) Int
+failEven = contramap even $ hoist (toEither "even") failTrue
+
+dup :: a -> (a, a)
+dup a = (a, a)
+
+combo :: FC (Either String) Int
+combo = divide dup failNegative failEven
